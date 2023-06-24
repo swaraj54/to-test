ee30a54a6d51954ac1878a277bd3fd81fe1193d5
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2023-06-24 20:53:01 +0530

    Updates

--- static/index.js
+++ static/index.js
@@ -14,3 +14,36 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+
+
+const express = require('express');
+const app = express();
+
+// Parse URL-encoded data
+app.use(express.urlencoded({ extended: true }));
+
+// Serve the HTML form
+app.get('/', (req, res) => {
+  res.send(`
+    <form method="POST" action="/submit">
+      <input type="text" name="username" placeholder="Username">
+      <input type="password" name="password" placeholder="Password">
+      <button type="submit">Submit</button>
+    </form>
+  `);
+});
+
+// Handle form submission
+app.post('/submit', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  
+  // Process the form data (e.g., validate, save to a database)
+
+  res.send(`Form submitted with username: ${username} and password: ${password}`);
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
